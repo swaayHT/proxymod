8360fc6e5f7461acb3353ed07c2d781a5c2ae944
swaayHT <kapxytibah@gmail.com>
2026-08-18 01:12:49 +0300

    Update ClientConnectionMixin.java

--- src/main/java/com/example/proxymod/mixin/ClientConnectionMixin.java
+++ src/main/java/com/example/proxymod/mixin/ClientConnectionMixin.java
@@ -8,17 +8,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin {
 
     @Inject(method = "connect", at = @At("HEAD"))
     private static void onConnect(InetSocketAddress address, boolean useEpoll, ClientConnection connection, CallbackInfo ci) {
-        if (ProxyMod.enabled && ProxyMod.host != null && !ProxyMod.host.isEmpty()) {
-            System.setProperty("socksProxyHost", ProxyMod.host);
-            System.setProperty("socksProxyPort", String.valueOf(ProxyMod.port));
-            ProxyMod.LOGGER.info("[ProxyMod] SOCKS5 proxy set to " + ProxyMod.host + ":" + ProxyMod.port);
-        }
+        ProxyMod.applyProxy();
     }
 }
