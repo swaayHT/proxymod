bd29812fb05d007a02c1e6b6fe9ad94336e986ac
swaayHT <kapxytibah@gmail.com>
2026-08-18 01:19:37 +0300

    Update ClientConnectionMixin.java

--- src/main/java/com/example/proxymod/mixin/ClientConnectionMixin.java
+++ src/main/java/com/example/proxymod/mixin/ClientConnectionMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.network.ClientConnection;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.net.InetSocketAddress;
 
@@ -13,7 +13,7 @@ import java.net.InetSocketAddress;
 public class ClientConnectionMixin {
 
     @Inject(method = "connect", at = @At("HEAD"))
-    private static void onConnect(InetSocketAddress address, boolean useEpoll, ClientConnection connection, CallbackInfo ci) {
+    private static void onConnect(InetSocketAddress address, boolean useEpoll, CallbackInfoReturnable<ClientConnection> cir) {
         ProxyMod.applyProxy();
     }
 }
